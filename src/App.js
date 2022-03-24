c5a51174ee6cc9fe846754907a4a44c8a24461a3
Robert Washbourne <robert@belmont.tech>
2022-03-24 02:02:35 -0400

    add pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
-function App() {
+function Child() {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  let { id } = useParams();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>ID: {id}</h3>
     </div>
   );
+}
+
+
+function App() {
+  const apphtml = <>
+    <p>
+      Edit and save to reload.
+    </p>
+    <div className='App-form'>
+      <input className="App-form-text" type="text" placeholder="Enter your name"></input>
+      <div><button>Play!</button></div>
+    </div>
+  </>;
+
+  return <div className="App">
+    <h1>Scrauwl</h1>
+    <div className="App-box">
+      <div className="App-container">
+        <Router>
+          <div>
+            <h2>Pages</h2>
+            <ul>
+              <li>
+                <Link to="/netflix">Netflix</Link>
+              </li>
+              <li>
+                <Link to="/the-goat">DaBaby</Link>
+              </li>
+              <li>
+                <Link to="/frank">Franklin</Link>
+              </li>
+              <li>
+                <Link to="/swamy">Swamy</Link>
+              </li>
+            </ul>
+            <Routes>
+              <Route path="/:id" element={<Child />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+    </div>
+  </div>;
 }
 
 export default App;
