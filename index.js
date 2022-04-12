cb4bf0a0cd7a4a3005d6e617780e0590bbf40107
Robert Washbourne <robert@belmont.tech>
2022-04-12 12:50:59 -0400

    add game logic with embedded db

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+import { nanoid } from 'nanoid'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { Low, JSONFile } from 'lowdb'
+import express from 'express'
+import { readFile } from 'fs'
+
+// Use JSON file for storage
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'db.json')
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+
+// read the wordlist from the file
+let wordlist;
+readFile(join(__dirname, 'wordlist.json'), (err, data) => {
+    if (err) throw err;
+    wordlist = JSON.parse(data);
+});
+
+// set default data
+await db.read();
+db.data ||= { games: [] }
+
 // express server
-const express = require('express');
 const app = express();
 const port = 3000;
-
 app.use(express.static('public'));
 
-// use socket.io for the realtime connection
-
-
+// later: use socket.io for the realtime connection
 // ### Operations
 // Game_ID: Unique id for each game
 // Player_ID: Unique id for each player
@@ -38,29 +58,116 @@ app.use(express.static('public'));
 //             Score
 
 // create a new game
-app.post('/game', (req, res) => {
+app.post('/newgame', async (req, res, next) => {
     // create ids for the game and start players empty
+    const newgame = {
+        gameID: nanoid(),
+        players: {},
+        words: {},
+    }
+    try {
+        db.data.games.push(newgame);
+        await db.write();
+        res.send(newgame.players);
+    } catch (error) {
+        return next(error);
+    }
+});
 
+// get a specific game by id
+app.get('/game/:game_id', async (req, res) => {
+    const game = db.data.games.find(game => game.gameID == req.params.game_id);
+    if (game) {
+        res.send(game.players);
+    } else {
+        res.status(404).send('Game not found');
+    }
+});
+
+// add a player to a game
+app.post('/game/:game_id/:player_id', async (req, res) => {
+    const game = db.data.games.find(game => game.gameID == req.params.game_id);
+    if (game) {
+        // get a random word for this player to draw
+        const random_word = wordlist[Math.floor(Math.random() * wordlist.length)];
+        const newplayer = {
+            canvas: "",
+            guesses: [],
+            score: 0
+        }
+        game.players[req.params.player_id] = newplayer;
+        game.words[req.params.player_id] = random_word;
+        await db.write();
+        res.send(newplayer);
+    } else {
+        res.status(404).send('Game not found');
+    }
 });
 
 // get the game state for a specific game id and player id
-app.get('/:game_id/:player_id', (req, res) => {
-    res.send('Hello World!');
+app.get('/game/:game_id/:player_id', async (req, res) => {
+    const game = db.data.games.find(game => game.gameID == req.params.game_id);
+    if (game) {
+        const player = game.players[req.params.player_id];
+        if (player) {
+            res.send(player);
+        } else {
+            res.status(404).send('Player not found');
+        }
+    } else {
+        res.status(404).send('Game not found');
+    }
 });
 
 // update the canvas for a specific game id and player id
-app.put('/:game_id/:player_id/canvas', (req, res) => {
-    res.send('Hello World!');
+app.put('/game/:game_id/:player_id/canvas', async (req, res) => {
+    const game = db.data.games.find(game => game.gameID == req.params.game_id);
+    if (game) {
+        const player = game.players[req.params.player_id];
+        if (player) {
+            player.canvas = req.body.canvas;
+            await db.write();
+            res.send(player);
+        } else {
+            res.status(404).send('Player not found');
+        }
+    } else {
+        res.status(404).send('Game not found');
+    }
 });
 
 // update the guesses for a specific game id and player id
-app.put('/:game_id/:player_id/guesses', (req, res) => {
-    res.send('Hello World!');
+app.put('/game/:game_id/:player_id/guess', async (req, res) => {
+    const game = db.data.games.find(game => game.gameID == req.params.game_id);
+    if (game) {
+        const player = game.players[req.params.player_id];
+        if (player) {
+            player.guesses.push(req.body.guess);
+            await db.write();
+            res.send(player);
+        } else {
+            res.status(404).send('Player not found');
+        }
+    } else {
+        res.status(404).send('Game not found');
+    }
 });
 
 // delete the game for a specific game id
-app.delete('/:game_id', (req, res) => {
-    res.send('Hello World!');
+app.delete('/game/:game_id', (req, res) => {
+    const game = db.data.games.find(game => game.gameID == req.params.game_id);
+    if (game) {
+        db.data.games.remove(game);
+        db.write();
+        res.send(game.players);
+    } else {
+        res.status(404).send('Game not found');
+    }
+});
+
+// doesnt match any route
+app.use((req, res) => {
+    res.status(404).send('Not found');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
