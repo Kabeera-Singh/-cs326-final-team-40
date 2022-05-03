4f113042a513e1293b9e57d1e201165c47c66249
Robert Washbourne <robert@belmont.tech>
2022-05-03 18:50:48 -0400

    cors

--- server/index.js
+++ server/index.js
@@ -28,12 +28,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // cors headers
-app.use("/game/*", function (req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
+app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+// cors headers
+app.use("/game/*", function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
     next();
 });
 
